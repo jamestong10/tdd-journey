f1cc826fd67fa07ddaaf5703a889093a6120cf19
JamesTung <jameskai17@gmail.com>
2019-07-23 00:03:05 +0800

    refactor test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -10,49 +10,49 @@ use App\User;
 class AuthTest extends TestCase
 {
     use WithFaker;
+    use RefreshDatabase;
+
     /**
      * @test
      * Test registration
      */
     public function testRegister()
     {
-        //User's data
-        $data = [
+        $userData = [
             'email' => $this->faker->email,
             'name' => $this->faker->name,
             'password' => 'secret1234',
             'password_confirmation' => 'secret1234',
         ];
-        //Send post request
-        $response = $this->json('POST', route('api.register'), $data);
-        //Assert it was successful
+
+        // send post request
+        $response = $this->json('POST', route('api.register'), $userData);
+        
+        // assert it was successful
         $response->assertStatus(200);
-        //Assert we received a token
+        
+        // assert we received a token
         $this->assertArrayHasKey('token', $response->json());
-        //Delete data
-        User::where('email', $data['email'])->delete();
     }
+    
     /**
      * @test
      * Test login
      */
     public function testLogin()
     {
-        //Create user
-        $user = User::create([
-            'name' => $this->faker->name,
-            'email'=> $this->faker->email,
-            'password' => bcrypt('secret1234')
-        ]);
-        //attempt login
+        $user = factory(User::class)->create();
+
+        // attempt login
         $response = $this->json('POST', route('api.authenticate'), [
             'email' => $user->email,
-            'password' => 'secret1234',
+            'password' => 'password',
         ]);
-        //Assert it was successful and a token was received
+        
+        // assert it was successful and a token was received
         $response->assertStatus(200);
+
+        // assert we received a token
         $this->assertArrayHasKey('token', $response->json());
-        //Delete the user
-        $user->delete();
     }
 }
--- tests/Feature/RecipeTest.php
+++ tests/Feature/RecipeTest.php
@@ -15,36 +15,32 @@ class RecipeTest extends TestCase
     use WithFaker;
     use RefreshDatabase;
 
-    //Create user and authenticate the user
+    /**
+     * Create user and authenticate the user
+     */
     protected function authenticate()
     {
-        $user = User::create([
-            'name' => $this->faker->name,
-            'email' => $this->faker->email,
-            'password' => Hash::make('secret1234'),
-        ]);
+        $user = factory(User::class)->create();
         $token = JWTAuth::fromUser($user);
         return compact('token', 'user');
     }
 
-    //Test the display all routes
+    /**
+     * Test the all route
+     */
     public function testAll()
     {
-        //Authenticate and attach recipe to user
         $authData = $this->authenticate();
         $token = $authData['token'];
-        $recipe = new Recipe([
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ]);
+        $recipe = factory(Recipe::class)->make();
         $user = $authData['user'];
         $user->recipes()->save($recipe);
-        //call route and assert response
+        
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. $token,
         ])->json('GET', route('recipe.all'));
-        $response->assertStatus(200);
         
+        $response->assertStatus(200);
         $this->assertEquals($recipe->title, $response->json()[0]['title']);
         $this->assertEquals($recipe->procedure, $response->json()[0]['procedure']);
     }
@@ -55,18 +51,18 @@ class RecipeTest extends TestCase
     public function testCreate()
     {
         $authData = $this->authenticate();
-        //Get token
         $token = $authData['token'];
+        $recipe = factory(Recipe::class)->make();
+
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. $token,
-        ])->json('POST', route('recipe.create'), [
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ]);
+        ])->json('POST', route('recipe.create'), $recipe->toArray());
+
         $response->assertStatus(200);
 
-        //Get count and assert
+        // get count and assert
         $user = $authData['user'];
+        $user->refresh();
         $count = $user->recipes()->count();
         $this->assertEquals(1, $count);
     }
@@ -77,69 +73,62 @@ class RecipeTest extends TestCase
     public function testUpdate()
     {
         $authData = $this->authenticate();
-        //Get token
         $token = $authData['token'];
-        $recipe = Recipe::create([
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ]);
+        list($recipe, $newRecipe) = factory(Recipe::class, 2)->make();
+        
         $user = $authData['user'];
         $user->recipes()->save($recipe);
-
-        $params = [
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ];
         
-        //call route and assert response
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. $token,
-        ])->json('POST', route('recipe.update', ['recipe' => $recipe->id]), $params);
+        ])->json('POST', route('recipe.update', ['recipe' => $recipe->id]), $newRecipe->toArray());
+
         $response->assertStatus(200);
 
-        //Assert title is the new title
-        $this->assertEquals($params['title'], $response->json()['title']);
-        $this->assertEquals($params['procedure'], $response->json()['procedure']);
+        // assert title is the new title
+        $this->assertEquals($newRecipe->title, $response->json()['title']);
+        $this->assertEquals($newRecipe->procedure, $response->json()['procedure']);
     }
 
-    //Test the single show route
+    /**
+     * Test the show route
+     */
     public function testShow()
     {
         $authData = $this->authenticate();
-        //Get token
         $token = $authData['token'];
-        $recipe = Recipe::create([
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ]);
+        $recipe = factory(Recipe::class)->make();
         $user = $authData['user'];
         $user->recipes()->save($recipe);
+
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. $token,
         ])->json('GET', route('recipe.show', ['recipe' => $recipe->id]));
+
         $response->assertStatus(200);
-        //Assert title is correct
+
+        // assert title is correct
         $this->assertEquals($recipe->title, $response->json()['title']);
     }
 
-    //Test the delete route
+    /**
+     * Test the delete route
+     */
     public function testDelete()
     {
         $authData = $this->authenticate();
-        //Get token
         $token = $authData['token'];
-        $recipe = Recipe::create([
-            'title' => $this->faker->name,
-            'procedure' => $this->faker->realText($this->faker->numberBetween(100, 200))
-        ]);
+        $recipe = factory(Recipe::class)->make();
         $user = $authData['user'];
         $user->recipes()->save($recipe);
         
         $response = $this->withHeaders([
             'Authorization' => 'Bearer '. $token,
         ])->json('POST', route('recipe.delete', ['recipe' => $recipe->id]));
+
         $response->assertStatus(200);
-        //Assert there are no recipes
+
+        // assert there are no recipes
         $this->assertEquals(0, $user->recipes()->count());
     }
 }
